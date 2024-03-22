08f1e42147a854340fa641f67ac1e51895b70147
Naposad <naposadik2@gmail.com>
2024-03-22 13:04:44 +0000

    First commit

---
+++ src/posts/urls.py
@@ -0,0 +1,12 @@
+from .views import BlogHome, BlogPostCreate, BlogPostUpdate, BlogPostDetail, BlogPostDelete
+from django.urls import path
+
+app_name = "posts"
+urlpatterns = [
+    path('', BlogHome.as_view(), name="home"),
+    path('<str:slug>', BlogPostDetail.as_view(), name="detail"),
+    path('create/', BlogPostCreate.as_view(), name="create"),
+    path('edit/<str:slug>', BlogPostUpdate.as_view(), name="edit"),
+    path('delete/<str:slug>', BlogPostDelete.as_view(), name="delete"),
+
+]
